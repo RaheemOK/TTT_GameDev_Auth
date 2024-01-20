b6a01e459d95126c2397b466379700dec4fdbdda
Raheem KHAN <raheemowaiskhan@raheems-air.home>
2024-01-20 23:50:05 +0000

    Testign testing

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ resource "google_compute_address" "static_address" {
   region = var.region
 }
 
-
 resource "google_compute_instance" "vm_instance" {
   name         = "ttt-gamedev-auth-micro-e2"
   machine_type = "e2-micro"
