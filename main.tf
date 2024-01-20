6d6f1a982527fbff9e0c407b12afd96ad1bfa911
Raheem KHAN <raheemowaiskhan@raheems-air.home>
2024-01-20 23:42:28 +0000

    Testign testing

--- main.tf
+++ main.tf
@@ -22,7 +22,8 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10-buster"
+      // Use Debian 10 Buster (amd64) image
+      image = "projects/debian-cloud/global/images/family/debian-10"
     }
   }
 
