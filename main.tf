2d2e62b4711775968355d1a535a680cd9a84fd56
Raheem KHAN <raheemowaiskhan@raheems-air.home>
2024-01-21 00:49:40 +0000

    Testign testing

--- main.tf
+++ main.tf
@@ -4,13 +4,19 @@ provider "google" {
 }
 
 resource "google_artifact_registry_repository" "my_repository" {
-  provider = google
-  location = var.region
+  provider     = google
+  location     = var.region
   repository_id = var.repository_id
-  format = "DOCKER"
+  format       = "DOCKER"
+}
+
+data "google_compute_address" "existing_static_address" {
+  name   = "vm-static-ip"
+  region = var.region
 }
 
 resource "google_compute_address" "static_address" {
+  count  = data.google_compute_address.existing_static_address.id == "" ? 1 : 0
   name   = "vm-static-ip"
   region = var.region
 }
@@ -22,7 +28,6 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      // Use Debian 10 Buster (amd64) image
       image = "projects/debian-cloud/global/images/family/debian-10"
     }
   }
@@ -30,16 +35,15 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.static_address.address
+      nat_ip = data.google_compute_address.existing_static_address.id != "" ? data.google_compute_address.existing_static_address.address : google_compute_address.static_address[0].address
     }
   }
 
   metadata = {
     "ssh-keys" = "raheem:${file("keygen.pub")}"
   }
-
 }
 
 output "vm_external_ip" {
-  value = google_compute_address.static_address.address
+  value = data.google_compute_address.existing_static_address.id != "" ? data.google_compute_address.existing_static_address.address : google_compute_address.static_address[0].address
 }
